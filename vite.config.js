35ebf3fe1144adee079b60bef917773ad4b8b4d9
De Roover Justin <justinderoover@gmail.com>
2023-11-08 13:42:18 +0100

    f

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import copy from "rollup-plugin-copy";
-import json from "@rollup/plugin-json"; // Importez le plugin rollup-plugin-json
 
 export default defineConfig({
   base: "/guess_game/", // Remplacez '/chemin-de-votre-application/' par la base souhaitée
@@ -17,9 +16,6 @@ export default defineConfig({
     copy({
       targets: [{ src: "assets", dest: "dist" }],
       hook: "writeBundle",
-    }),
-    json({
-      path: "assets/gameIndex.json",
     }),
   ],
 });
